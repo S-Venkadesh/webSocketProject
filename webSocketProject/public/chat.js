31c058eed98a57bb860f960b829e24c0e273d92d
Venkadesh S <venkadeshvarman21154@gmail.com>
2020-11-26 13:27:47 +0530

    Add files via upload

---
+++ webSocketProject/public/chat.js
@@ -0,0 +1,26 @@
+
+var message = document.getElementById('message');
+var handle=document.getElementById('handle');
+var btn=document.getElementById('send');
+var output=document.getElementById('output');
+var feedback=document.getElementById('feedback');
+
+btn.addEventListener('click',function(){
+    socket.emit('chat',{
+        message:message.value,
+        handle:handle.value
+    })
+})
+
+message.addEventListener('keypress',function(){
+    socket.emit('typing',handle.value);
+})
+
+socket.on('chat',function(data){
+    feedback.innerHTML=""
+    output.innerHTML += "<p><strong> 👨 " + data.handle + " :  📲</strong>" + data.message + "</p>"
+})
+
+socket.on('typing',function(data){
+    feedback.innerHTML = "<p><em>" + data + " is typing a message..." + "</em></p>"
+})
